12a6c229c4a07e0c0f13e8936c08cda1e702d653
Security <Security.Varna@bg.nestle.com>
2020-05-29 13:42:02 +0300

    Fixed typo

--- Mapping-Components-Practice/App.js
+++ Mapping-Components-Practice/App.js
@@ -22,7 +22,7 @@ function App() {
 
     const productComponents = productsData.map(item => {
       // pass props as single obj !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      <Prodcut key={item.id} product={item} />
+      <Product key={item.id} product={item} />
     });
   );
 
