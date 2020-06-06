660f26f5bace44f8f19e0a13252b67b5204b9020
Security <Security.Varna@bg.nestle.com>
2020-06-06 10:31:44 +0300

    Added Statefull Components

---
+++ Handling-Events-in-React/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {};
+//   }
+//   render() {
+//     return (
+//       <div></div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
