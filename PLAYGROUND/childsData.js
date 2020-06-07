d954557078ed14f12e71357017381de9d836b4d3
Developer <kraevit@gmail.com>
2020-06-07 23:49:44 +0300

    Added Phase-6

---
+++ PLAYGROUND/childsData.js
@@ -0,0 +1,19 @@
+const childsData = [
+  {
+    id: 1,
+    name: "Boss",
+    age: 10
+  },
+  {
+    id: 2,
+    name: "Chief",
+    age: 11
+  },
+  {
+    id: 3
+    name: "Employee",
+    age: 20
+  }
+];
+
+export default childsData;
